d1c6260988cae14c37ce5cb040f33c5cec7b49e7
Bertrand Caron <bertrand@bcaron.me>
2014-09-05 09:13:31 +0200

    Added User scaffold
    - As given in Chapter 6 and 7 of M.Hartl Tutorial :  https://www.railstutorial.org/book/

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :email, :password_digest, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904180655) do
+ActiveRecord::Schema.define(version: 20140904183140) do
 
   create_table "internships", force: true do |t|
     t.text     "title"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 20140904180655) do
 
   create_table "uploaded_files", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
