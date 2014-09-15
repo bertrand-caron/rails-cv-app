632c09e61a3e534e8ae69c23e81467aa825a508e
Bertrand Caron <bertrand@bcaron.me>
2014-09-15 16:39:03 +0200

    Add external YAML config file
    - Move custom information (name, emails, etc.) to dedicated YAML
    config file for easier maintaining

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ActionMailer::Base
   def send_email(email, content)
     @email = email
     @content = content
-    mail(from: @email, to: "admin@bcaron.me", subject: "New mail from research.bcaron.me")
+    mail(from: @email, to: Settings['email'], subject: "New mail from research.bcaron.me")
   end
 end
