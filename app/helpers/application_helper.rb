e0e3592462e92c779eaca02d7ccaa3861c69ae9c
Bertrand Caron <bertrand.fx.caron@gmail.com>
2014-10-04 11:01:04 +0200

    DRY up the index views of my models
    - Introduce three application-wide helper functions :
    edit_tool_tag, delete_tool_tag and star_tag
    => DRY up views with them
    - Fixed a little CSS inconsistency in margins for programmin_skill's
    markdown-jumbotron

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,14 @@ module ApplicationHelper
 
     markdown.render(text).html_safe
   end
+
+  #DRY version of the .edit-tools selectors code
+  def edit_tool_tag(edit_path)
+    link_to '<i class="fa fa-pencil-square-o"></i>'.html_safe, edit_path
+  end
+
+  #DRY version of the .edit-tools selectors code
+  def delete_tool_tag(model)
+    link_to '<i class="fa fa-close"></i>'.html_safe, model, method: :delete, data: { confirm: 'Are you sure?' }
+  end
 end
