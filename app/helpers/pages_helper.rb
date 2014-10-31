18fcf84be36d34015b2be44025097e898959b415
Bertrand Caron <bertrand.fx.caron@gmail.com>
2014-10-31 20:12:53 +0100

    Empty Sections will not be displayed to unlogged users
    - As such, people can just leave unwanted sections alone

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+
+  def empty_section?(models_name)
+    # Taken from http://stackoverflow.com/questions/14070369/how-to-instantiate-class-from-name-string-in-rails
+    not Object.const_get(models_name.gsub(/ /,'').singularize).any?
+  end
 end
