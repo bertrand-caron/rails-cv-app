49f1c3399f7f349df7ff85c69d0f3a127b092d5c
Bertrand Caron <bertrand.fx.caron@gmail.com>
2014-11-01 12:35:59 +0100

    Add User Settings help text
    - Using a hash stored in the controller#edit method, displays custom
      help text to each defined settings (if available in the hash)

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -9,6 +9,23 @@ class SettingsController < ApplicationController
 
   # GET /settings/edit
   def edit
+    @help_hash = {
+      :full_name => "Your full name. Ex: 'John Doe'",
+      :page_title => "The title of your Résumé. Ex: 'Curriculum Vitae' or 'CV'",
+      :main_occupation => "Your main occupation or title. Ex: 'Aero-spatial Engineer'",
+      :alt_occupation => "Alternate occupation title displayed when hovering over your main occupation. Leave empty to disable. Ex: 'Your next hire ?'",
+      :contact_email => "Your contact email, to which the contact emails will be forwarded. Ex: 'john.doe@jdoe.me'",
+      :mobile_phone => "Your mobile phone number.",
+      :address => "Your contact physical mail address.",
+      :infos => "Any additional information can be fitted here. Ex: 'Driving Licence' or '27 years old'",
+      :github_name => "Your Github name. Leave empty to disable.",
+      :stackoverflow_id => "Your StackOverflow account id. Leave empty to disable.",
+      :stackoverflow_name => "Any name you would like to see displayed. Leave empty to disable.",
+      :linkedIn_alias => "Your LinkedIn page, without the 'https://www.linkedin.com/in/' part. Ex: 'johndoe'. Leave empty to disable.",
+      :twitter_alias => "Your Twitter alias, without the @. Ex: 'John_Doe'. Leave empty to disable.",
+      :main_color => "Hexadecimal color (without the pound '#' symbol) used for your Name, as well as for the titles of the sections. Ex: 'F09819'",
+      :secondary_color => "Hexadecimal color (without the pound '#' symbol) used for your for the titles of the items. Ex: '428BCA'"
+    }
   end
 
   # PATCH/PUT /settings/edit
