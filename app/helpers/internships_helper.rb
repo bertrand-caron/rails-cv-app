81996351c57031f5d7a9cf7c302b7e25943c1a27
Bertrand Caron <bertrand.fx.caron@gmail.com>
2014-11-02 17:31:41 +0100

    HOTFIX : Fix App crash when missing referee_id for new internships
    - Fix with has_referee? helper + if statement

--- app/helpers/internships_helper.rb
+++ app/helpers/internships_helper.rb
@@ -1,2 +1,10 @@
 module InternshipsHelper
+  def has_referee?(referee_id)
+    begin
+      Referee.find(referee_id)
+      true
+    rescue ActiveRecord::RecordNotFound
+      false
+    end 
+  end
 end
