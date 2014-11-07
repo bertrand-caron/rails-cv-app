fc7cb9b8834d60f0dd63603a969f8940d6dd0938
Bertrand Caron <bertrand.fx.caron@gmail.com>
2014-11-07 09:38:41 +0100

    Modify skill :icon field to accept both fa and mfizz icons
    - Outsource the logic to an helper function icon_tag_for
    - Updated help-block to provide target='blank' links to both libraries
    
    WARNING: Old ProgrammingSkills used an implicit scheme where the 'icon-'
    part of the font-mfizz was asumed
    => Necessary to update them manually after git:pull

--- app/helpers/skills_helper.rb
+++ app/helpers/skills_helper.rb
@@ -2,4 +2,13 @@ module SkillsHelper
   def star_tag(item)
     raw item.star_tag
   end
+
+  # Help produce a tag from a single name from both font-awesome and font-mfizz
+  def icon_tag_for(icon_name)
+    if icon_name =~ /\Afa/
+      content_tag 'i', nil,:class => "fa #{icon_name}"
+    elsif icon_name =~ /\Aicon/
+      content_tag 'i', nil,:class => icon_name
+    end
+  end
 end
