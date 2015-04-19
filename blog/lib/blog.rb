ded5b77c8543c8c5adc91523d86ed60d8f7d0709
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-04-19 11:57:52 +1000

    Add a (namespaced) YAML config to the Blog Engine

--- blog/lib/blog.rb
+++ blog/lib/blog.rb
@@ -4,6 +4,8 @@ require 'font-awesome-rails'
 require 'jquery-rails'
 require 'twitter-bootstrap-rails'
 require 'redcarpet'
+require 'yaml'
 
 module Blog
+  APP_CONFIG = YAML.load_file("#{Blog::Engine.root}/config/config.yml")
 end
