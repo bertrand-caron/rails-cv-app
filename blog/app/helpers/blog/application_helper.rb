cbed1615b71d0fe551f444aa01fc5514b1c3ee7d
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-04-19 11:28:02 +1000

    Start rendering Markdown

--- blog/app/helpers/blog/application_helper.rb
+++ blog/app/helpers/blog/application_helper.rb
@@ -1,5 +1,13 @@
 module Blog
   module ApplicationHelper
     include FontAwesome::Rails::IconHelper
+
+    def markdown(text, options={})
+      renderer = Redcarpet::Render::HTML.new()
+      markdown_renderer = Redcarpet::Markdown.new(renderer, extensions = {})
+      # options = {downgrade: n} Downgrade Markdown heading by n levels for better rendering in the context of the page
+      markdown_renderer.render(options.key?(:downgrade) ? text.gsub(/(#)+/, '\&###') : text).html_safe
+    end
+
   end
 end
