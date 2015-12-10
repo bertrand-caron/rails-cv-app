edaab239a3ac56449ca2ff94d06e093a3c5b85b4
Bertrand Caron <bertrand.fx.caron@gmail.com>
2015-12-11 08:27:53 +1000

    Add a link to the blog, if the blog is enabled

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Load global configurations
 Settings = YAML.load_file("#{Rails.root}/config/config.yml")
+Settings['cv_or_blog_url'] = Settings['use-blog-engine'] ? '/blog' : '/'
 
 # Load global variable AVAILABLE_SECTIONS
 AVAILABLE_SECTIONS = Settings['available-sections'].split(' ')
